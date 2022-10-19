e74a45aae580623e56587510c94ce110c228bac1
suraj0004 <surajupadhyaa@gmail.com>
2022-10-20 01:28:15 +0530

    add: trie implementation - insert - find - delete

--- Trees/trie.js
+++ Trees/trie.js
@@ -18,16 +18,29 @@ class TrieNode {
     this.children["*"] = new TerminatingTrieNode();
   }
 
-  hasChild(char){
-    return this.children.hasOwnProperty(char)
+  hasChild(char) {
+    return this.children.hasOwnProperty(char);
   }
 
-  getChild(char){
+  getChild(char) {
     return this.children[char];
   }
 
-  hasTerminationNode(){
-    return this.children.hasOwnProperty("*")
+  hasTerminationNode() {
+    return this.children.hasOwnProperty("*");
+  }
+
+  hasChildren() {
+    if(this.hasTerminationNode()){
+        return Object.keys(this.children).length > 1;
+    }
+    return Object.keys(this.children).length > 0;
+  }
+
+  deleteChild(char) {
+    if (this.children.hasOwnProperty(char)) {
+      delete this.children[char];
+    }
   }
 }
 
@@ -50,27 +63,41 @@ class Trie {
     parent.insertTerminatingNode();
   }
 
-  wordExist(word){
+  wordExist(word) {
     let parent = this.root;
-    for(let i = 0; i < word.length; i++){
-        if(!parent.hasChild(word[i])){
-            return false;
-        }
-        parent = parent.getChild(word[i])
+    for (let i = 0; i < word.length; i++) {
+      if (!parent.hasChild(word[i])) {
+        return false;
+      }
+      parent = parent.getChild(word[i]);
     }
 
     return parent.hasTerminationNode();
   }
 
-  checkPrefix(prefix){
+  checkPrefix(prefix) {
     let parent = this.root;
-    for(let i = 0; i < prefix.length; i++){
-        if(!parent.hasChild(prefix[i])){
-            return false;
-        }
-        parent = parent.getChild(prefix[i])
+    for (let i = 0; i < prefix.length; i++) {
+      if (!parent.hasChild(prefix[i])) {
+        return false;
+      }
+      parent = parent.getChild(prefix[i]);
     }
     return true;
+  }
+
+  deleteWord(word) {
+    this.wordExist(word) && this.#delete(this.root, word, 0);
+  }
+
+  #delete(parent, word, i) {
+    if (i < word.length) {
+      let child = parent.getChild(word[i]);
+      this.#delete(child, word, i + 1);
+      if (!child.hasChildren()) {
+        parent.deleteChild(word[i]);
+      }
+    }
   }
 }
 
@@ -81,14 +108,19 @@ function main() {
   trie.insert("nupur");
   trie.insert("honey");
   trie.insert("noes");
-  trie.insert("hores");
-  trie.insert("sun");
-  trie.insert("suri");
-  trie.insert("surajupp");
+  //   trie.insert("hores");
+  //   trie.insert("sun");
+  //   trie.insert("suri");
+  //   trie.insert("surajupp");
 
   console.clear();
-  console.log( trie.wordExist('su') );
-  console.log( trie.checkPrefix('su') );
+  console.log(trie.wordExist("su"));
+  console.log(trie.checkPrefix("su"));
+
+  console.log(JSON.stringify(trie.root));
+  trie.deleteWord("noes");
+  console.log('\n');
+  console.log(JSON.stringify(trie.root));
 }
 
 main();
